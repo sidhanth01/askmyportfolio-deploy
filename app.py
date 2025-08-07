9750f72cc1a473dd3032aa5640d8877feb8c379c
sidhanth01 <sidhanth.l05@gmail.com>
2025-08-07 16:16:30 +0530

    rerun correct

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ if not REPLICATE_API_TOKEN:
 replicate_client = replicate.Client(api_token=REPLICATE_API_TOKEN)
 
 # Choose your Replicate model
-REPLICATE_MODEL = "meta/llama-3-8b"
+REPLICATE_MODEL = "meta/meta-llama-3-8b"
 
 def call_replicate_llm(prompt: str) -> str:
     try:
