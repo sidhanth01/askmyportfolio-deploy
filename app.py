1ce0caa121d08c5694cd0203cca12d27c8bbfff4
Sidhanth L <151510127+sidhanth01@users.noreply.github.com>
2025-10-24 22:05:45 +0530

    Update import for ChatPromptTemplate module

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ except Exception:
 
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 
 import requests  # For Together AI API calls
 import re
